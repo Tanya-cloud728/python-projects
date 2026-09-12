481d5c23cab8a5ddecd74320329fb14c5c0d8c32
TANYA KAUR <tanyakaur@TANYAs-MacBook-Air-2.local>
2026-09-12 22:16:17 +0530

    Organize Python projects

---
+++ dice rolling system/ver2.py
@@ -0,0 +1,8 @@
+import random
+want=input("do u want to play again? yes or no-->")
+while(want=="yes"): 
+    choose=random.randint(1,6)
+    print(choose)
+    want=input("do u want to play or not")
+  
+
